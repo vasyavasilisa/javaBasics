c2fcee173107e5a9a92ed757507d24052240fdc3
vasilisa <vasilisademuanova2700@gmail.com>
2019-09-08 14:24:49 +0300

    add 4 classes with tasks

---
+++ src/firstProject/Lesson1Task1.java
@@ -0,0 +1,14 @@
+package firstProject;
+
+public class Lesson1Task1 {
+
+	public static void main(String[] args) {
+		  byte[] byteArray = {1,2,3,4,5};
+		  int sum = 0;
+		  for (byte b : byteArray){
+			  sum += b;
+		  }
+			System.out.println(sum);
+	}
+
+}
