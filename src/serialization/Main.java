ff295c0737931769fc1e59f05b8ad0a429258146
vasilisa <vasilisademuanova2700@gmail.com>
2019-10-31 19:53:20 +0300

    delete file

--- src/serialization/Main.java
+++ src/serialization/Main.java
@@ -7,23 +7,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		Object object = new Object();
-		object.setIntValue(12);
+		object.setIntValue(18);
 		object.setName("JKJLLKJLK gfgdf");
 		Object object2 = new Object();
-		object2.setIntValue(13);
+		object2.setIntValue(2);
 		object2.setName("JKJK");
 		Object object3 = new Object();
 		object3.setIntValue(10);
 		object3.setName("JLK");
 		Object object4 = new Object();
-		object4.setIntValue(1);
+		object4.setIntValue(7);
 		object4.setName("J");
 		List<Object> list = new ArrayList<>();
 		list.add(object);
 		list.add(object2);
 		list.add(object3);
 		list.add(object4);
-		//new ObjectKeeper().saveObjectList(list);
+		new ObjectKeeper().saveObjectList(list);
 		System.out.println(new ObjectKeeper().readObjectList());
 	}
 
